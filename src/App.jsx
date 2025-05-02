1ef76fadd55cfeffc9d891128b3417e0c5ebd34b
Keerthana <168490508+skrti@users.noreply.github.com>
2025-05-01 23:00:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
       width="70%"
       margin="0 auto"
     >
-      <Heading level={1}>My Profile</Heading>
+      <Heading level={1}>Thanks for login</Heading>
 
       <Divider />
 
